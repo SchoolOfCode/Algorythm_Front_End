79a8fd07893ae85cd5e852eb7fabf911f4750d5d
Fowl3r <90485594+Fowl3r@users.noreply.github.com>
2023-03-30 11:09:20 +0100

    postContract functiona added to post data to backend when backend URL incorporated

--- AlgoRhythm/src/App.jsx
+++ AlgoRhythm/src/App.jsx
@@ -1,13 +1,31 @@
 import InputForm from './components/InputForm'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [contracts, setContracts] = useState([])
+
+// POST request handling
+const postContract = async obj => {
+  const contract = await fetch(`database URL`, {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  });
+  const data = await contract.json();
+  console.log(data);
+  setContracts([...contracts, data])
+
+}
 
 
   return (
     <>
     <h1>React App</h1>
-    <InputForm />
+    <InputForm postContract={postContract} />
     </>
   )
 }
