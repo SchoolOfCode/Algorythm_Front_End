2a8fdddd8ef02b33c0a2614479df0e07c8f60aa3
Fowl3r <90485594+Fowl3r@users.noreply.github.com>
2023-03-30 10:59:35 +0100

    input form complete using react-hook-form to capture data, not styled

---
+++ AlgoRhythm/src/components/InputForm.jsx
@@ -0,0 +1,62 @@
+import {useForm} from "react-hook-form";
+
+
+export default function InputForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm({})
+  return (
+    <form
+    onSubmit={handleSubmit(data => {
+        console.log(data)
+    })}
+    >
+    <input
+    type="date"
+    placeholder="Start Date"
+    {...register('startDate', {required: 'Start Date is required'})}
+     />
+     <p>{errors.startDate?.message}</p>
+     <input
+    type="date"
+    placeholder="End Date"
+    {...register('endDate', {required: 'End Date is required'})}
+     />
+     <p>{errors.endDate?.message}</p>
+     <input
+    type="text"
+    placeholder="Contact"
+    {...register('contact', {required: 'contact is required'})}
+     />
+     <p>{errors.contact?.message}</p>
+     <input
+    type="text"
+    placeholder="Contract name"
+    {...register('contractName', {required: 'contract name is required'})}
+     />
+     <p>{errors.contractName?.message}</p>
+     <input
+    type="text"
+    placeholder="Role"
+    {...register('role', {required: 'Role is required'})}
+     />
+     <p>{errors.role?.message}</p>
+     <select
+     {...register("skill", {required: 'please select a skill'})}
+     >
+    <option value="">Skill</option>
+    <option value="JavaScript">JavaScript</option>
+    <option value="TypeScript">TypeScript</option>
+    <option value="AWS">AWS</option>
+    <option value="PostGreSQL">PostGreSQL</option>
+     </select>
+    <p>{errors.skill?.message}</p>
+    <input type="submit" />
+
+
+{/* contract name, role name, skills needed - dropdown */}
+    </form>
+  )
+}
